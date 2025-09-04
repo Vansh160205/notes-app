2ea2a17ee921a0ae92fd78c4d6d8ddf555fe674e
vansh-vagadia <vanshvagadia1602@gmail.com>
2025-09-04 20:29:54 +0530

    Update .env file

--- backend/notes/src/main/java/com/example/notes/NotesApplication.java
+++ backend/notes/src/main/java/com/example/notes/NotesApplication.java
@@ -2,10 +2,10 @@ package com.example.notes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class NotesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NotesApplication.class, args);
 	}
